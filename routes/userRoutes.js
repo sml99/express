413db9c53ba088b2b23f4299e558639052e6da2f
sml99 <smailovdahmani@gmail.com>
2022-01-26 12:13:36 +0100

    remote mongodb config

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const userController = require('../controllers/userController');
+
 const router = express.Router();
-const userController = require('./../controllers/userController');
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
